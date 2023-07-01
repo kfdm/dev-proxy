55c38709df0679b327d9c95cb13d2b3aa3bdbfb0
Paul Traylor <kungfudiscomonkey@gmail.com>
2023-07-01 16:50:02 +0900

    Second draft with splitting the methods up

--- devserver/cli.py
+++ devserver/cli.py
@@ -1,19 +1,22 @@
-from http.server import HTTPServer
+import logging
+from http.server import ThreadingHTTPServer
 
 import click
 
 from .proxy import ProxyHTTPRequestHandler
 
+logger = logging.getLogger(__name__)
+
 
 @click.group()
 def cli():
-    pass
+    logging.basicConfig(level=logging.WARNING)
 
 
 @cli.command()
 @click.option("--server", default="localhost")
 @click.option("--port", default=8080)
 def web(server, port):
-    httpd = HTTPServer((server, port), ProxyHTTPRequestHandler)
-    print("http server is running", httpd.server_port)
+    httpd = ThreadingHTTPServer((server, port), ProxyHTTPRequestHandler)
+    logger.info("http server is running %s", httpd.server_port)
     httpd.serve_forever()
--- devserver/proxy.py
+++ devserver/proxy.py
@@ -1,8 +1,11 @@
 from http.server import BaseHTTPRequestHandler
 from subprocess import STDOUT, check_call
 from threading import Semaphore
-
+import logging
 from requests import ConnectionError, HTTPError, Session
+
+
+logger = logging.getLogger(__name__)
 
 configs = {
     "tsun.localhost": {
@@ -14,42 +17,56 @@ configs = {
 }
 
 
-class ProxyHTTPRequestHandler(BaseHTTPRequestHandler):
-    protocol_version = "HTTP/1.0"
-
-    def do_GET(self, body=True):
+class ProxyRequest(BaseHTTPRequestHandler):
+    def lookup_host(self, **kwargs):
         try:
-            host = self.headers["Host"]
+            self.process_host(host=self.headers["Host"], **kwargs)
+        except KeyError:
+            self.send_error(500, "Missing Host Header")
+
+    def process_host(self, host, **kwargs):
+        try:
             config = configs[host]
         except KeyError:
             self.send_error(500, f"Unknown host: {host}")
-            return
+        else:
+            with config.get("lock", Semaphore()):
+                self.process_request(host=host, config=config, **kwargs)
 
+    def process_request(self, host, config, **kwargs):
         client = Session()
+        try:
+            response = client.get(
+                url=f"http://localhost:{config['port']}{self.path}",
+                headers=self.headers,
+            )
+            response.raise_for_status()
+        except HTTPError:
+            self.send_error(response.status_code)
+        except ConnectionError:
+            check_call(
+                [
+                    "tmux",
+                    "new-session",
+                    "-s",
+                    config["name"],
+                    "-d",
+                    config["command"],
+                ],
+                cwd=config["cwd"],
+                stderr=STDOUT,
+            )
+            self.send_error(500, "Service not running yet")
+        else:
+            self.send_response(response.status_code)
+            self.wfile.write(response.content)
 
-        with config.get("lock", Semaphore()):
-            try:
-                response = client.get(
-                    url=f"http://localhost:{config['port']}{self.path}",
-                    headers=self.headers,
-                )
-                response.raise_for_status()
-            except HTTPError:
-                self.send_error(response.status_code)
-            except ConnectionError:
-                check_call(
-                    [
-                        "tmux",
-                        "new-session",
-                        "-s",
-                        config["name"],
-                        "-d",
-                        config["command"],
-                    ],
-                    cwd=config['cwd'],
-                    stderr=STDOUT,
-                )
-                self.send_error(500, "Service not running yet")
-            else:
-                self.send_response(response.status_code)
-                self.wfile.write(response.content)
+
+class ProxyHTTPRequestHandler(ProxyRequest):
+    protocol_version = "HTTP/1.0"
+
+    def do_GET(self, body=True):
+        self.lookup_host(method="GET")
+
+    def do_POST(self, body=True):
+        self.lookup_host(method="POST")
