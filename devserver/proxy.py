f5e2ddfc7e233de7307c815fcfe367d97ceff6dc
Paul Traylor <kungfudiscomonkey@gmail.com>
2023-07-07 18:43:53 +0900

    Watch config file for changes
    We can use a `async for changes` statement at the end of our main, so
    that we can watch for changes to our config file. This allows us to
    automatically watch for changes.
    
    We put a `self.servicelock` around our reading/writing to ensure we do
    not get any weirdness if we were to reload a configuration at the same
    time a request is getting processed.

--- devserver/proxy.py
+++ devserver/proxy.py
@@ -1,33 +1,19 @@
-import asyncio
 import logging
+from asyncio import locks
 from pathlib import Path
 
 from aiohttp import client, web
+from watchfiles import awatch
 
 from . import config, upstream
 
 logger = logging.getLogger(__name__)
 
 
-config_map = config.load(config.DEFAULT)
-
-configs = {
-    config_map["service"][name]["host"]: upstream.HostConfig(
-        name=name, config=config_map["service"][name]
-    )
-    for name in config_map["service"]
-}
-
-
 class ProxyDispatch:
     def __init__(self, fn: Path = config.DEFAULT):
-        self.config = config.load(fn)
-        self.services = {
-            config_map["service"][name]["host"]: upstream.HostConfig(
-                name=name, config=config_map["service"][name]
-            )
-            for name in config_map["service"]
-        }
+        self.config_file = fn
+        self.servicelock = locks.Lock()
 
     async def process(self, request: web.Request, config: upstream.HostConfig):
         logger.debug("Processing: %s %s%s", request.method, request.host, request.path)
@@ -43,14 +29,28 @@ class ProxyDispatch:
 
     async def handler(self, request: web.Request):
         try:
-            config = self.services[request.host]
+            async with self.servicelock:
+                config = self.services[request.host]
         except KeyError:
             logger.warning("Unknown host: %s%s", request.host, request)
             return web.Response(status=500, text=f"Unknown host: {request.host}")
         else:
             return await self.process(request, config)
 
+    async def load(self, config_file: Path):
+        async with self.servicelock:
+            logger.info("Loading config %s", config_file)
+            self.config = config.load(config_file)
+            self.services = {
+                self.config["service"][name]["host"]: upstream.HostConfig(
+                    name=name, config=self.config["service"][name]
+                )
+                for name in self.config["service"]
+            }
+
     async def main(self, host, port):
+        await self.load(self.config_file)
+
         server = web.Server(self.handler)
         runner = web.ServerRunner(server)
         await runner.setup()
@@ -59,6 +59,6 @@ class ProxyDispatch:
 
         logger.info("=== Serving on http://%s:%s ===", host, port)
 
-        # pause here for very long time by serving HTTP requests and
-        # waiting for keyboard interruption
-        await asyncio.sleep(100 * 3600)
+        async for changes in awatch(self.config_file):
+            for path in changes:
+                await self.load(Path(path[1]))
