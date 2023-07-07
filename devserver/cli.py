b9211b8dbf2c72938baf38c3b6fe20bcfb98ea6a
Paul Traylor <kungfudiscomonkey@gmail.com>
2023-07-07 19:11:17 +0900

    Specify config path on the CLI

--- devserver/cli.py
+++ devserver/cli.py
@@ -1,9 +1,10 @@
 import asyncio
 import logging
+from pathlib import Path
 
 import click
 
-from . import proxy
+from . import config, proxy
 
 logger = logging.getLogger(__name__)
 
@@ -15,14 +16,15 @@ def cli():
 
 
 @cli.command()
-@click.option("--server", default="localhost")
-@click.option("--port", default=8080)
-def web(server, port):
+@click.option("--config", default=config.DEFAULT, type=Path, show_default=True)
+@click.option("--host", default="localhost", show_default=True)
+@click.option("--port", default=8080, show_default=True)
+def web(config, **kwargs):
     loop = asyncio.get_event_loop()
 
     try:
-        proxy_server = proxy.ProxyDispatch()
-        loop.run_until_complete(proxy_server.main(server, port))
+        proxy_server = proxy.ProxyDispatch(config)
+        loop.run_until_complete(proxy_server.main(**kwargs))
     except KeyboardInterrupt:
         pass
     finally:
